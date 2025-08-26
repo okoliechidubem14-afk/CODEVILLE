18327a6415a210d43940a2c1fb8406b49865eaae
okoliechidubem14-afk <okoliechidubem14@gmail.com>
2025-08-26 11:36:15 +0100

    My Portfolio

---
+++ Introduction to Javascript/script.js
@@ -0,0 +1,41 @@
+
+// exercise
+let my_hobby = "gaming";
+my_hobby = "reading";
+// 
+let studentNumber = 42;
+let studentName = "Jacob";
+let isStudent = true;
+let studentAge;
+let studentId = []
+
+let subjects = ["Math", "Science", "History", "English", "Art", "Physical Education"];
+console.log(subjects)
+// how to access first item on the array
+console.log(subjects[0]);
+// how to access last item on the array
+console.log(subjects[subjects.length - 1]);
+// how to get the total number of items in the array
+console.log(subjects.length);
+// how to update an item in the array
+subjects[0] = "Biology";
+console.log(subjects);
+
+let bestMoivie= {name: "Spiderman: No Way Home",
+                  year: "2021",
+                  director: "Jon Watts",
+                  actor: "Tom Holland",
+                };
+                
+console.log(bestMoivie);
+
+// conditionals
+
+let isSunny = true;
+if (isSunny){
+    console.log;("go out freely");
+}
+
+else{
+    console.log("take an umbrella");
+}
